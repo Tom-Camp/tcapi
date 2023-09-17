c248713ec956a55c6ab27e02160bfc2da1ffb4da
Tom Camp <tomcamp@protonmail.ch>
2023-09-17 18:29:15 -0400

    Feature/cors (#1)
    * Adding pre-commit checks.
    
    * Adding CORS middleware.

--- app/db.py
+++ app/db.py
@@ -6,6 +6,8 @@ from app.config import settings
 
 database = databases.Database(settings.db_url)
 metadata = sqlalchemy.MetaData()
+
+
 class BaseMeta(ormar.ModelMeta):
     metadata = metadata
     database = database
@@ -18,6 +20,7 @@ class User(ormar.Model):
     id: int = ormar.Integer(primary_key=True)
     email: str = ormar.String(max_length=128, unique=True, nullable=False)
     active: bool = ormar.Boolean(default=True, nullable=False)
+
 
 engine = sqlalchemy.create_engine(settings.db_url)
 metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.db import database, User
+from app.db import User, database
 
 app = FastAPI(title="Tom.Camp")
 
+origins = [
+    "http://tcapi.localhost",
+    "https://tcapi.localhost",
+    "http://tcapi.localhost:8008",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
